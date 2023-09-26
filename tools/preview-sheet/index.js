8be979a335b67d36d3509623eb06bc365698d056
Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro-6.local>
2023-09-26 19:52:17 +0530

    Adding channel Publish button in sidekick

--- tools/preview-sheet/index.js
+++ tools/preview-sheet/index.js
@@ -1,13 +1,26 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+
 const fetchData = async (url, option = {}) => {
   let result = '';
   try {
     result = fetch(url)
-      .then((response) => {
+        .then((response) => {
           if (!response.ok) {
-              throw new Error(`request to fetch ${url} failed with status code ${response.status}`);
+            throw new Error(`request to fetch ${url} failed with status code ${response.status}`);
           }
           return response.text();
-      });
+        });
     return Promise.resolve(result);
   } catch (e) {
     throw new Error(`request to fetch ${url} failed with status code with error ${e}`);
@@ -45,17 +58,15 @@ const previewAndRedirect = async () => {
   const repo = params.get("repo");
   const owner = params.get("owner");
   const referrer = params.get("referrer");
-  const previewHost = `https://${ref}--${repo}--${owner}.hlx.page`;
   const statusUrl = `https://admin.hlx.page/status/${owner}/${repo}/${ref}?editUrl=${referrer}`;
   console.log('location is ' + window.location.href);
-  console.log("previewHost = " + previewHost);
   console.log("statusUrl = " + statusUrl);
   const status = JSON.parse(await fetchData(statusUrl));
   console.log("status is " + JSON.stringify(status));
   if(status.preview && status.preview.url) {
-      console.log("status.preview.url = " + status.preview.url);
+    console.log("status.preview.url = " + status.preview.url);
   } else {
-      console.log("preview URL no present");
+    console.log("preview URL not present");
   }
 
 
@@ -63,10 +74,15 @@ const previewAndRedirect = async () => {
   const url = new URL(previewUrl);
   const sheetPath = url.pathname;
   console.log("sheetPath is " + sheetPath);
-  const pagePath = `${sheetPath.slice(0, -17)}main`;//remove .json
+  const pagePath = `${sheetPath.slice(0, -17)}main`;//remove recognitions.json
   await previewAndCacheClear(owner, repo, ref, sheetPath);
   await previewAndCacheClear(owner, repo, ref, pagePath);
-  window.location.replace(pagePath);
+
+  const configUrl =  `https://admin.hlx.page/sidekick/${owner}/${repo}/${ref}/config.json`;
+  const config = JSON.parse(await fetchData(configUrl));
+  const previewHost = config.previewHost;
+
+  window.location.replace(`https://${previewHost}${pagePath}`);
 }
 
 previewAndRedirect();
---
+++ tools/sidekick/plugin.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+const sk = document.querySelector('helix-sidekick');
+sk.addEventListener('custom:publish-channel', async (e) => {
+    if(e.detail) {
+        console.log(JSON.stringify(e.detail));
+    } else {
+        console.log("details not available");
+    }
+    let response;
+    const options = {
+        method: 'POST',
+    };
+    const config = e.detail.data.config;
+    const ref = config.ref;
+    const repo = config.repo;
+    const owner = config.owner;
+    const host = config.host;
+    const status = e.detail.data.status;
+    const path = status.webPath;
+
+    response = await fetch(`https://admin.hlx.page/live/${owner}/${repo}/${ref}/${path}`, options);
+
+    if (response.ok) {
+        console.log(`Document Published at ${new Date().toLocaleString()}`);
+    } else {
+        throw new Error(`Could not previewed. Status: ${response.status}`);
+    }
+
+
+    response = await fetch(`https://admin.hlx.page/cache/${owner}/${repo}/${ref}/${path}`, options);
+
+    if (response.ok) {
+        console.log(`Purge cache ${new Date().toLocaleString()}`);
+    } else {
+        throw new Error(`Could not purge cache. Status: ${response.status}`);
+    }
+
+    const sheetPath = `${path}.json`;
+
+    response = await fetch(`https://admin.hlx.page/live/${owner}/${repo}/${ref}/${sheetPath}`, options);
+
+    if (response.ok) {
+        console.log(`Document Published at ${new Date().toLocaleString()}`);
+    } else {
+        throw new Error(`Could not previewed. Status: ${response.status}`);
+    }
+
+    response = await fetch(`https://admin.hlx.page/cache/${owner}/${repo}/${ref}/${sheetPath}`, options);
+
+    if (response.ok) {
+        console.log(`Purge cache ${new Date().toLocaleString()}`);
+    } else {
+        throw new Error(`Could not purge cache. Status: ${response.status}`);
+    }
+
+    const prodUrl = `https://${host}${path}`;
+    window.location.href = prodUrl;
+});
