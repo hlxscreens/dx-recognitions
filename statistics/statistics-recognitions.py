ca14d55fdeafb29ff2e1117a3f8e30e8b42027f0
Anuj Mahajan <anujmaha@adobe.com>
2024-02-24 11:20:48 +0530

    fix: updating statistics for all orgs

--- statistics/statistics-recognitions.py
+++ statistics/statistics-recognitions.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 def fetch_json_data(url):
     response = requests.get(url)
@@ -11,21 +12,24 @@ def fetch_json_data(url):
         return None
 
 def analyze_recognitions(data):
+    current_date = datetime.now().strftime("%d/%m/%Y")
     total_recognitions = data['total']
     recognitions = data['data']
 
     active_recognitions = sum(1 for recognition in recognitions
-                              if recognition['Start Date'] <= '24/02/2024' and recognition['End Date'] >= '24/02/2024')
+                            if not recognition.get('Start Date') or recognition['Start Date'] <= current_date
+                            and (not recognition.get('End Date') or recognition['End Date'] >= current_date))
+    custom_image_urls = sum(1 for recognition in recognitions if recognition['Image URL'])
 
-    descriptions_over_50 = sum(1 for recognition in recognitions if len(recognition['Description']) > 50)
+    descriptions_over_50 = sum(1 for recognition in recognitions if len(recognition['Description'].split()) > 50)
 
-    non_empty_image_urls = sum(1 for recognition in recognitions if recognition['Image URL'])
+    no_end_date_recognitions = sum(1 for recognition in recognitions if not recognition.get('End Date'))
 
-    return total_recognitions, active_recognitions, descriptions_over_50, non_empty_image_urls
+    return total_recognitions, active_recognitions, custom_image_urls, descriptions_over_50, no_end_date_recognitions
 
-def plot_data(org_name, total_recognitions, active_recognitions, descriptions_over_50, non_empty_image_urls):
-    labels = ['Total Recognitions', 'Active Recognitions', 'Descriptions > 50', 'Non-empty Image URLs']
-    values = [total_recognitions, active_recognitions, descriptions_over_50, non_empty_image_urls]
+def plot_data(org_name, total_recognitions, active_recognitions, custom_image_urls, descriptions_over_50, no_end_date_recognitions):
+    labels = ['Total Recognitions', 'Active Recognitions', 'Descriptions > 50', 'Custom Image URLs']
+    values = [total_recognitions, active_recognitions, descriptions_over_50, custom_image_urls]
 
     plt.figure(figsize=(10, 6))
     plt.bar(labels, values, color=['blue', 'green', 'orange', 'red'])
@@ -34,17 +38,29 @@ def plot_data(org_name, total_recognitions, active_recognitions, descriptions_ov
     plt.ylabel('Counts')
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.savefig(f'{org_name}_statistics.png')
+    plt.savefig(f'statistics/{org_name}-statistics.png')
     plt.show()
 
 def main():
-    org_url = "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-anup/recognitions.json"
-    data = fetch_json_data(org_url)
+    org_urls = [
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-anup/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-balaji/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-gaurav/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-gitesh/all/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-manoj/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-sanjay-kaluskar/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-sanjay-kumar/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-suvrat/recognitions.json",
+        "https://dx-recognitions.aem-screens.net/content/screens/org-amitabh/org-vikas/recognitions.json"
+    ]
 
-    if data:
-        total_recognitions, active_recognitions, descriptions_over_50, non_empty_image_urls = analyze_recognitions(data)
-        org_name = "org-anup"  # Change this accordingly
-        plot_data(org_name, total_recognitions, active_recognitions, descriptions_over_50, non_empty_image_urls)
+    for org_url in org_urls:
+        data = fetch_json_data(org_url)
+
+        if data:
+            total_recognitions, active_recognitions, custom_image_urls, descriptions_over_50, no_end_date_recognitions = analyze_recognitions(data)
+            org_name = org_url.split('/')[-2]  # Extract org name from URL
+            plot_data(org_name, total_recognitions, active_recognitions, custom_image_urls, descriptions_over_50, no_end_date_recognitions)
 
 if __name__ == "__main__":
     main()
