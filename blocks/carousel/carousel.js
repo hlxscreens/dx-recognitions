f0a51ec8b801ee5d6e4962e0ca55b81eaa438992
dhavkuma <89851739+dhavkuma@users.noreply.github.com>
2024-11-22 16:52:34 +0530

    fix: set dashboard duration to 60s (#16)

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -11,6 +11,14 @@ const DEFAULT_HEADING = 'DX India Recognitions';
 const NO_HEADING = 'No Heading';
 // 1 image - 400x400, 2 images - 330x330, 3 images - 273x273, 4 images - 256x256, 5 images - 227x227
 const IMAGE_SIZES = ['20.6vw', '16.9vw', '14vw', '13.2vw', '11.8vw'];
+
+const DEFAULT_ITEM_DURATION = 10 * 1000; // 10 seconds
+const DEFAULT_DASHBOARD_ITEM_DURATION = 60 * 1000; // 60 seconds
+let itemDuration = DEFAULT_ITEM_DURATION;
+
+const DASHBOARDS_BLOCK_NAME = 'dashboards';
+const CAROUSEL_ITEM_DASHBOARDS_CLASS = `carousel-item-${DASHBOARDS_BLOCK_NAME}`;
+
 const TIMEOUTS = {
   timeouts: [],
   setTimeout(fn, delay) {
@@ -219,9 +227,7 @@ async function buildCarouselForDashboard(block) {
   const carouselItems = [];
   childDivs?.forEach((div) => {
     const carouselItem = createDivWithClass('carousel-item');
-    carouselItem?.classList.add('carousel-item-analytics');
-    carouselItem?.setAttribute('start-date', '23/09/2024');
-    carouselItem?.setAttribute('end-date', '23/09/2030');
+    carouselItem?.classList.add(CAROUSEL_ITEM_DASHBOARDS_CLASS);
 
     const link = div.querySelector('a');
     if (link) {
@@ -235,22 +241,7 @@ async function buildCarouselForDashboard(block) {
     }
     carouselItems.push(carouselItem);
   });
-  return carouselItems;
-}
-
-async function buildCarouselItems(block) {
-  buildCarouselForDashboard(block);
-  const assets = block.querySelectorAll('picture');
-  const carouselItems = [];
-  assets.forEach((asset) => {
-    const carouselItem = createDivWithClass('carousel-item');
-    carouselItem.classList.add('carousel-item-analytics');
-    carouselItem.setAttribute('start-date', '23/09/2024');
-    carouselItem.setAttribute('end-date', '23/09/2030');
-    carouselItem.appendChild(asset.cloneNode(true));
-
-    carouselItems.push(carouselItem);
-  });
+  itemDuration = DEFAULT_DASHBOARD_ITEM_DURATION;
   return carouselItems;
 }
 
@@ -265,7 +256,7 @@ export default async function decorate(block) {
   if (block.classList.contains('recognitions')) {
     const items = await buildCarouselFromSheet(block);
     main.querySelector('.carousel-track').append(...items);
-  } else if (block.classList.contains('analytics')) {
+  } else if (block.classList.contains(DASHBOARDS_BLOCK_NAME)) {
     const items = await buildCarouselForDashboard(block);
     main.querySelector('.carousel-track').append(...items);
   } else {
@@ -276,7 +267,6 @@ export default async function decorate(block) {
   const carouselItems = carouselTrack.querySelectorAll('.carousel-item');
   const totalItems = carouselItems.length;
   let currentIndex = -1;
-  const DEFAULT_ITEM_DURATION = 10 * 1000;
 
   if (totalItems === 0) {
     return;
@@ -311,7 +301,7 @@ export default async function decorate(block) {
       nextSlide();
     } else {
       showSlide(currentIndex);
-      TIMEOUTS.setTimeout(nextSlide, DEFAULT_ITEM_DURATION);
+      TIMEOUTS.setTimeout(nextSlide, itemDuration);
     }
   }
 
