bf05aae61d4602d13585c7e7adc52f571b0c9174
Satnam Singh <satnams@Satnams-Laptop.local>
2025-11-22 16:12:20 +0530

    fix: plain.html were missing

--- scripts/generators/dashboards.js
+++ scripts/generators/dashboards.js
@@ -177,6 +177,9 @@ export default class HtmlGenerator {
       additionalAssets.push('/blocks/carousel/utils.js');
       additionalAssets.push('/blocks/carousel/carousel.css');
 
+      // Add the dashboard page's own .plain.html file
+      additionalAssets.push(`${path}.plain.html`);
+
       await fs.ensureDir(p.dirname(path));
       await fs.outputFile(`${path}.html`, $.html());
     } catch (error) {
